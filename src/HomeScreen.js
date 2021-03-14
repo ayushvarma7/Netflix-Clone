14058c0963fe86b9892f354f357397f1855f7024
ayushvarma7 <57842615+ayushvarma7@users.noreply.github.com>
2021-03-14 19:07:15 +0530

    Movies populated

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -3,6 +3,8 @@ import React from 'react';
 import './HomeScreen.css';
 import Navbar from './Navbar';
 import Banner from './Banner';
+import Row from './Row';
+import requests from './Requests';
 
 const Homescreen = () => {
     return (
@@ -10,7 +12,18 @@ const Homescreen = () => {
             {/* Navbar */}
             <Navbar/>
         <Banner/>
-            {/* Rows */}
+            <Row 
+            title='NETFLIX ORIGINALS'
+            fetchUrl={requests.fetchNetflixOriginals}
+            isLargeRow
+             />
+            <Row title='TRENDING NOW' fetchUrl={requests.fetchTrending}/>
+            <Row title='Top Rated' fetchUrl={requests.fetchTopRated}/>
+            <Row title='Action Movies' fetchUrl={requests.fetchActionMovies}/>
+            <Row title='Come    dy Movies' fetchUrl={requests.fetchComedyMovies}/>
+            <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies}/>
+            <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies}/>
+            <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries}/>
         </div>
     )
 }
