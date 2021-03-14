1d373e410613afb1810836329f38f285aed900c0
ayushvarma7 <57842615+ayushvarma7@users.noreply.github.com>
2021-03-14 14:41:04 +0530

    Movie Database Added

--- src/Banner.js
+++ src/Banner.js
@@ -1,32 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from './axios';
 
 import './Banner.css';
+import requests from './Requests';
 function Banner() {
-
+   
+    const [movie, setMovie]= useState([]);
     function truncate(string, n){ //truncate function
         return string?.length>n ? string.substr(0, n-1) + '...' : string;
     }
 
+    useEffect(()=>{
+        async function fetchData(){
+            const request= await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[   //this 'request' is the data we got from the get request
+                Math.floor(Math.random()* request.data.results.length-1)
+                ]
+            );
+            return request;
+        }
+        fetchData();
+    }, []);
+
+    console.log(movie);
+
     return (
         <header className="banner" style={{
             backgroundSize: "cover",
-            backgroundImage:   `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+            backgroundImage:`url(" https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}")`,
             backgroundPosition: "center center"
         }}
         >
           <div className="banner__contents">
-              <h1 className="banner__title">Movie Name</h1>
+              <h1 className="banner__title">
+                  {movie?.title || movie?.name || movie?.original_name}
+              </h1>
               <div className="banner__buttons">
                   <button className="banner__button">Play</button>
                   <button className="banner__button">My List</button>
               </div>
               <h1 className="banner__description">{
-                  truncate(`This is a test description This is a test descriptionThis is a test description
-                  This is a test description
-                  This is a test description
-                  This is a test description
-                  This is a test description
-                  `, 150)}</h1>
+                  truncate(`${movie?.overview}`, 150)}</h1>
           </div>  
 
           <div className="banner--fadeBottom"/> 
@@ -36,3 +51,6 @@ function Banner() {
 }
 
 export default Banner;
+
+// https://image.tmbd.org/t/p/original/${movie?.backdrop_path}
+// https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png
