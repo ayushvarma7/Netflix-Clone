e715303d70605e4cb8e20f469104b8412abc53f4
ayushvarma7 <57842615+ayushvarma7@users.noreply.github.com>
2021-03-16 17:23:22 +0530

    Persistance added during authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,10 +10,26 @@ import LoginScreen from './screens/LoginScreen';
 
 
 import './App.css';
+import { auth } from './firebase';
 
 
 export const App = () => {
   const user = null;
+
+  useEffect(() => { //checks for if the user is logged in 
+    const unsubscribe= auth.onAuthStateChanged((userAuth)=>{
+      if(userAuth){
+        //Logged In
+        console.log(userAuth);
+      }else{
+        //Logged out
+      }
+    })
+    return unsubscribe; //detaching the onAuth listner
+    // by detaching it prevents the creation of duplicate elements of unsubscribe
+    // i.e basically prevents reducing the efficiency
+  }, [])
+
   return (
     <div className="app">
       <Router>
