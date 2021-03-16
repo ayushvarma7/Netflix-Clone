337154e64fb9d457102a50f75f0ac46acb34c3f3
ayushvarma7 <57842615+ayushvarma7@users.noreply.github.com>
2021-03-16 17:03:49 +0530

    Firebase authentication added

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAyEGVPHSKAhuyRmBuTE5fbU5eFtS8Vd5o",
     authDomain: "netflix-clone-f9ea0.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "55675019625",
     appId: "1:55675019625:web:e33456158e2eed9b241df8"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db= firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export {auth };  //this is an explicit export( no limit on number of explicit exports)
+  export default db;
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { auth } from '../firebase';
 
 import './SignupScreen.css';
 
 
 function SignupScreen() {
+    const emailRef= useRef(null);
+    const passwordRef= useRef(null); 
+    //useState renders on each change, useRef is silent (doesn't re-render)
 
 const register =(e)=>{
     e.preventDefault();
-}
+
+    auth.createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+    ).then((authUser)=>{ //do, if promise persists i.e create user and log the details
+        console.log(authUser);
+    }).catch((error)=>{ //if the promise fails
+        alert(error.message);
+    });
+};
 
 const signIn=(e)=>{
     e.preventDefault(); //prevents refresh 
+
+    auth.signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+    ).then((authUser)=>{ // i.e signIn the user and log the details
+        console.log(authUser);
+    }).catch((error)=>{ //if not, alert!
+        alert(error.message);
+    });
 } 
 
     return (
-        <div className="signupScreen">
+        <div className="signupScreen"> 
             <form>
                 <h1>Sign In</h1>
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password"/>
+                <input ref={emailRef} type="email" placeholder="Email" />
+                <input ref={passwordRef} type="password" placeholder="Password"/>
                 <button type="submit" onClick={signIn}>Sign In</button>
                 <h4> <span className="signupScreen__gray"> New to Netflix? </span>
                <span className="signupScreen__link" onClick={register}> Sign Up now.</span>
