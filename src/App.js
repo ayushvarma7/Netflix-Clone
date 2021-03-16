bbea5e761954bbdcf822dcf656504a6a3c17d81a
ayushvarma7 <57842615+ayushvarma7@users.noreply.github.com>
2021-03-16 19:48:33 +0530

    Redux Functionality Added

--- src/App.js
+++ src/App.js
@@ -11,18 +11,28 @@ import LoginScreen from './screens/LoginScreen';
 
 import './App.css';
 import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 
 
 export const App = () => {
-  const user = null;
+  // const user = null;
+  const user= useSelector(selectUser);  //this selectUser returns the user and logs in then
+  const dispatch= useDispatch();
 
   useEffect(() => { //checks for if the user is logged in 
     const unsubscribe= auth.onAuthStateChanged((userAuth)=>{
       if(userAuth){
         //Logged In
-        console.log(userAuth);
+        // console.log(userAuth);
+        dispatch(login({
+          uid: userAuth.uid,
+          email:userAuth.email,
+        })
+        );
       }else{
         //Logged out
+        dispatch(logout);
       }
     })
     return unsubscribe; //detaching the onAuth listner
